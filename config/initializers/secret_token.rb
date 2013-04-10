6f021331c39b778334d720d215eb44589150215f
alexanderkjones <alexanderkjones@gmail.com>
2013-04-10 15:37:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HiveDataStore::Application.config.secret_token = 'd6209cdae05c97abecac08f8140fc7b26947139940b0f2052ccce028662eb7605fafea3ee83ca6620b91e4995be488168c176f8bb5eaca78ff6051eb39a64812'
